a7abd4f93eb40987a7ecfd553ab6522aaa145935
Aik Siang <ang.aik.siang@gmail.com>
2014-05-04 16:24:51 +0300

    changed phone filter to data

--- src/android/PickContactPlugin.java
+++ src/android/PickContactPlugin.java
@@ -28,7 +28,7 @@ public class PickContactPlugin extends CordovaPlugin {
 		if (action.equals("chooseContact")) {
 
             Intent intent = new Intent(Intent.ACTION_PICK,
-                     ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                     ContactsContract.Data.CONTENT_URI);
             cordova.startActivityForResult(this, intent, CHOOSE_CONTACT);
 
             PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
